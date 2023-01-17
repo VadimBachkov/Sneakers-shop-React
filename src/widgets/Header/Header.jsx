12143f8db9dd558afcd20fba06429553eb99522b
Vadim_B <bachkovvadim@gmail.com>
2023-01-17 23:29:59 +0300

    FE01:Create header

--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -1,9 +1,33 @@
+import Logo from 'assets/HeaderImg/Logo.svg';
+import Cart from 'assets/HeaderImg/Cart.svg';
+import Liked from 'assets/HeaderImg/Liked.svg';
+ 
+import style from 'widgets/Header/style.module.css';
+
 function Header() {
-    return (
-      <div>
-        Header
+  return (
+    <div className={style.headerWrapper}>
+      <div className={style.logoWrapper}>
+        <img className={style.logo} src={Logo} alt="Logo" />
+        <div className={style.titleWrapper}>
+          <div className={style.title}>REACT SNEAKERS</div>
+          <div className={style.secondaryTitle}>Best sneaker store</div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Header;
+      <div className={style.cartWrapper}>
+        <div className={style.cart}>
+          <img src={Cart} alt="Cart" />
+        </div>
+        <div className={style.orderSum}>555 BYN</div>
+        <div className={style.liked}>
+          <img src={Liked} alt="Liked" />
+        </div>
+        <div className={style.user}>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
