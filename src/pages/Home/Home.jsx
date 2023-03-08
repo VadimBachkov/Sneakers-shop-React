dc63a2c3688022b3ed3bae5c017b49d4372c1195
Vadim_B <bachkovvadim@gmail.com>
2023-03-08 21:17:24 +0300

    +

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,33 +11,33 @@ import { Link } from "react-router-dom";
 import { ROUTES } from "shared/consts";
 
 function Home() {
-    useEffect(() => {
-        ProductModel.fetch();
-    }, []);
+  useEffect(() => {
+    ProductModel.fetch();
+  }, []);
 
-    return (
+  return (
       <div className={style.menuWrapper}>
         <div className={style.menuTitle}>All Sneakers</div>
         <button>Profile
-          <Link to={ROUTES.POSTS}></Link>
+          <Link to={ROUTES.PROFILE}></Link>
         </button>
         <div className={style.productsWrapper}>
-             {ProductModel.loading ? (
-          <>Loading....</>
-        ) : (
+          {ProductModel.loading ? (
+            <>Loading....</>
+          ) : (
             ProductModel.products.map((item) => (
-            <ProductCard
-            image={item.image}
-            title={item.title}
-            description={item.description}
-            price={item.price}
-            key={item.title}
-          />
-        ))
-      )}
+              <ProductCard
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                price={item.price}
+                key={item.title}
+              />
+            ))
+          )}
         </div>
       </div>
-    )
+  )
 }
 
 export default observer(Home);
--- src/shared/consts/routes.js
+++ src/shared/consts/routes.js
@@ -1,5 +1,5 @@
 export const ROUTES = {
     HOME: '/',
     POSTS: '/posts',
-    PROFILE: '/profile'
+    PROFILE: '/profile',
 }
